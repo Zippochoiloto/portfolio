866a9d50f3741d500363f1fdf5108370becf09ff
TheAnh <theanhptit94@gmail.com>
2020-04-06 18:14:32 +0700

    6/4/2020

--- public/image/portfolio/main.js
+++ public/image/portfolio/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $(".main-toggler").on("click", function() {
+    $(this).toggleClass("open");
+    $(".top-nav").toggleClass("open");
+  });
 
+  $("top-nav .nav-link").on("click", function() {
+    $("menu-toggler").removeClass("open");
+    $("top-nav").removeClass("open");
+  });
+
+  $('nav a[href = "#"]').on("click", function() {
+    $("html, body").animate();
+  });
 });
